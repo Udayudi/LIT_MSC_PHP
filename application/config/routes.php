f49cbf02d3c7edaef5ea75ce9d1715372d6c7a90
naveenkoli <kolinaveen02@gmail.com>
2021-07-23 16:02:43 +0530

    updated htaccess file

--- application/config/routes.php
+++ application/config/routes.php
@@ -54,7 +54,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $route['(:any)'] = 'pages/view/$1';
 
-
 $route['default_controller'] = 'pages/view';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/main.css">
-    <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/custom.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/main.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/custom.css">
     <title>Local Issues Tracker</title>
 </head>
 <body>
